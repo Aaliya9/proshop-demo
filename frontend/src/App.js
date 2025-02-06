44642c5b56191ecf9611282f5305542810db2678
Aaliya9 <aaliyasharif1998@gmail.com>
2025-02-06 11:56:29 +0530

    header footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
+import { Container } from 'react-bootstrap'
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 import React from 'react'
 
 const App = () => {
   return (
-    <h1>
-      Welcome to ProShop
-    </h1>
+    <>
+   <Header/>
+   <main className='py-3'>
+   <Container>
+   <h1>Welcome to Proshop</h1>
+   </Container>
+    </main>
+    <Footer/>
+    </>
   )
 }
 
