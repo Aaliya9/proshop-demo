95859ba57e8176f0008b1589bcd4f95ae234e78c
Aaliya9 <aaliyasharif1998@gmail.com>
2025-02-13 15:09:06 +0530

    routes middleware and controllers added

---
+++ backend/controllers/productController.js
@@ -0,0 +1,26 @@
+import asynchandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
+
+//@desc    Fetch all products
+//@route   GET /api/products
+//@access  Public
+const getProducts = asynchandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+});
+
+
+//@desc    Fetch a product
+//@route   GET /api/products/:id
+//@access  Public
+const getProductById = asynchandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+        return res.json(product);
+    } else {
+        res.status(404);
+        throw new Error('Resource not found');
+    }
+});
+
+export { getProducts, getProductById };
